59c274df9f7fda8312e31eac21244e59dd84a377
bergurtareqtamimi <bergurt19@ru.is>
2021-12-09 19:36:36 +0000

    Beginning of day 8

--- day8/seve_segment.py
+++ day8/seve_segment.py
@@ -1,4 +1,16 @@
 
+def read_input():
+    coll = []
+    with open("inp2.txt") as file:
+        line = file.readline()
+        while line:
+            left = sorted(["".join(sorted(x)) for x in line.strip().split("|")[0].split()])
+            right = ["".join(sorted(x)) for x in line.strip().split("|")[1].split()]
+            coll.append([left, right])
+            line = file.readline()
+    return coll
+
+
 def segment_to_possible_numbers(segment):
     if len(segment) == 2:
         return 1
@@ -12,6 +24,7 @@ def segment_to_possible_numbers(segment):
         return 2, 3, 5
     if len(segment) == 6:
         return 6, 9, 0
+
 
 def number_to_segments(number):
     if number == 0:
@@ -34,6 +47,26 @@ def number_to_segments(number):
         return "a", "b","c", "d", "f", "g", "e"
     if number == 9:
         return "a", "b","c", "d", "f", "g"
+
+def get_mapper():
+    d = dict()
+    for i in ["a", "b", "c","d","e","f","g"]:
+        d[i] = []
+    return d
+
+data = read_input()
+print(data)
+counter = 0
+for case in data:
+    mapper = get_mapper()
+    for number in case[0]:
+        p_number = segment_to_possible_numbers(number)
+
+        if p_number in [1,4,7,8]:
+            counter += 1
+
+print(counter)
+
 
 # take in each segment (before pipe) and convert them into possible numbers
 # convert each possible number into possible segments
